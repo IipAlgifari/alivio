ee63dc761194f2da33a4b31e2ba4d1f830d51211
Iip M Algifari <139673662+IipAlgifari@users.noreply.github.com>
2024-03-10 02:30:36 +0700

    menambahkan file data product js

--- src/presentation/component/organism/SectionProduct/index.jsx
+++ src/presentation/component/organism/SectionProduct/index.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
-
-import { initAnimation } from "core/util/animate/animate";
-
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { initAnimation } from 'core/util/animate/animate';
+import productData from './productData'; // Impor data produk
 
 const SectionProduct = () => {
   // --- onScroll
   useEffect(() => {
     initAnimation();
   }, []);
+
   return (
     <div className="product">
       <div className="container">
         <div className="product__txt" data-aos="fade-left" data-aos-duration="2300">
-          <h6 className="product__subtitle">our product</h6>
-          <h3 className="product__title">You tell us your stress, We make your diary</h3>
-          <p className="product__desc">Everyone experiences it, and in different ways.<br/>Let Alivio guide
-           you, in a personalized journal experience, to overcome your stress.</p>
-          <Link to="#" className="btn">Find Your Way</Link>
+          <h6 className="product__subtitle">{productData.subtitle}</h6>
+          <h3 className="product__title">{productData.title}</h3>
+          <p className="product__desc">{productData.description}</p>
+          <Link to="#" className="btn">{productData.buttonText}</Link>
         </div>
         <div className="product__wrapper">
           <div className="product__img" >
-            <img className="product__img__el" src="asset/images/dummy/Image1.jpg" data-aos="fade-up" data-aos-duration="1700" alt="product_of_the_image_profile" />
+            <img className="product__img__el" src={productData.thumbnail.imgSrc} data-aos="fade-up" data-aos-duration="1700" alt={productData.thumbnail.altText} />
           </div>
           <div className="product__thumbnail" data-aos="fade-up-right" data-aos-duration="18000">
-            <h4 className="thumbnail-title">Customized For</h4>
-            <img className="thumbnail-img" src="asset/images/dummy/Image1.jpg" alt="product img profile"/>
-            <h5 className="thumbnail-name">Mikayla</h5>
+            <h4 className="thumbnail-title">{productData.thumbnail.title}</h4>
+            <img className="thumbnail-img" src={productData.thumbnail.imgSrc} alt={productData.thumbnail.altText} />
+            <h5 className="thumbnail-name">{productData.thumbnail.name}</h5>
             <div>
-              <h3 className="thumbnail-subtitle">Your custom contents</h3>
+              <h3 className="thumbnail-subtitle">{productData.thumbnail.subtitle}</h3>
               <ul className="thumbnail-list">
-                <li className="thumbnail-list-item">Understand Your Stress</li>
-                <li className="thumbnail-list-item">Ang er Management</li>
+                {productData.thumbnail.list.map((item, index) => (
+                  <li key={index} className="thumbnail-list-item">{item}</li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SectionProduct
+export default SectionProduct;
---
+++ src/presentation/component/organism/SectionProduct/productData.js
@@ -0,0 +1,19 @@
+const productData = {
+  subtitle: "our product",
+  title: "You tell us your stress, We make your diary",
+  description: "Everyone experiences it, and in different ways. Let Alivio guide you, in a personalized journal experience, to overcome your stress.",
+  buttonText: "Find Your Way",
+  thumbnail: {
+    title: "Customized For",
+    imgSrc: "asset/images/dummy/Image1.jpg",
+    altText: "product img profile",
+    name: "Mikayla",
+    subtitle: "Your custom contents",
+    list: [
+      "Understand Your Stress",
+      "Anger Management"
+    ]
+  }
+};
+
+export default productData;
